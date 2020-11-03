52def1c745568d180e7765ab88eca911b5e99466
Judd Morgan <6760345+jumo2@users.noreply.github.com>
2020-11-03 13:45:54 -0600

    removed hardcoded values for inital state of availableNums and candidateNums

--- star-match-game/src/App.js
+++ star-match-game/src/App.js
@@ -24,8 +24,8 @@ const PlayNumber = props => (
 const StarMatch = () => {
 
   const [stars, setStars] = useState(utils.random(1,9));
-  const [availableNums, setAvailableNums] = useState([1,2,3,4,5]);
-  const [candidateNums, setCandidateNums] = useState([2,3]);
+  const [availableNums, setAvailableNums] = useState(utils.range(1,9));
+  const [candidateNums, setCandidateNums] = useState([]);
 
   const candidatesAreWrong = utils.sum(candidateNums) > stars;
 
@@ -38,7 +38,7 @@ const StarMatch = () => {
     }
     return 'available';
 
-  }
+  };
 
 
 
